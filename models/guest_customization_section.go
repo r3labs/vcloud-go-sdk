76d5882a3b1ec6ea7586b1b307c9494201d1c336
Tom Bevan <tom.bevan@r3labs.io>
2017-11-23 17:12:12 +0000

    allowing setting/getting guest customization script, workaround to support escaped xml characters

--- models/guest_customization_section.go
+++ models/guest_customization_section.go
@@ -6,28 +6,46 @@ package models
 
 // GuestCustomizationSection ...
 type GuestCustomizationSection struct {
-	Type                  string       `xml:"type,attr,omitempty"`
-	Href                  string       `xml:"href,attr,omitempty"`
-	Info                  *GenericElem `xml:"Info,omitempty"`
-	Description           string       `xml:"Description,omitempty"`
-	Enabled               bool         `xml:"Enabled"`
-	ChangeSid             string       `xml:"ChangeSid,omitempty"`
-	VirtualMachineID      string       `xml:"VirtualMachineId,omitempty"`
-	JoinDomainEnabled     bool         `xml:"JoinDomainEnabled"`
-	DomainName            string       `xml:"DomainName,omitempty"`
-	DomainUserName        string       `xml:"DomainUserName,omitempty"`
-	DomainUserPassword    string       `xml:"DomainUserPassword,omitempty"`
-	MachineObjectOU       string       `xml:"MachineObjectOU,omitempty"`
-	UseOrgSettings        bool         `xml:"UseOrgSettings"`
-	AdminPasswordEnabled  bool         `xml:"AdminPasswordEnabled"`
-	AdminPasswordAuto     bool         `xml:"AdminPasswordAuto"`
-	AdminPassword         string       `xml:"AdminPassword,omitempty"`
-	AdminAutoLogonEnabled bool         `xml:"AdminAutoLogonEnabled"`
-	AdminAutoLogonCount   string       `xml:"AdminAutoLogonCount"`
-	ResetPasswordRequired bool         `xml:"ResetPasswordRequired"`
-	ComputerName          string       `xml:"ComputerName"`
-	CustomizationScript   string       `xml:"CustomizationScript,omitempty"`
-	Link                  *Link        `xml:"Link"`
+	Type                  string               `xml:"type,attr,omitempty"`
+	Href                  string               `xml:"href,attr,omitempty"`
+	Info                  *GenericElem         `xml:"Info,omitempty"`
+	Description           string               `xml:"Description,omitempty"`
+	Enabled               bool                 `xml:"Enabled"`
+	ChangeSid             string               `xml:"ChangeSid,omitempty"`
+	VirtualMachineID      string               `xml:"VirtualMachineId,omitempty"`
+	JoinDomainEnabled     bool                 `xml:"JoinDomainEnabled"`
+	DomainName            string               `xml:"DomainName,omitempty"`
+	DomainUserName        string               `xml:"DomainUserName,omitempty"`
+	DomainUserPassword    string               `xml:"DomainUserPassword,omitempty"`
+	MachineObjectOU       string               `xml:"MachineObjectOU,omitempty"`
+	UseOrgSettings        bool                 `xml:"UseOrgSettings"`
+	AdminPasswordEnabled  bool                 `xml:"AdminPasswordEnabled"`
+	AdminPasswordAuto     bool                 `xml:"AdminPasswordAuto"`
+	AdminPassword         string               `xml:"AdminPassword,omitempty"`
+	AdminAutoLogonEnabled bool                 `xml:"AdminAutoLogonEnabled"`
+	AdminAutoLogonCount   string               `xml:"AdminAutoLogonCount"`
+	ResetPasswordRequired bool                 `xml:"ResetPasswordRequired"`
+	ComputerName          string               `xml:"ComputerName"`
+	CustomizationScript   *CustomizationScript `xml:"CustomizationScript,omitempty"`
+	Link                  *Link                `xml:"Link"`
+}
+
+// CustomizationScript ...
+type CustomizationScript struct {
+	Value string `xml:",innerxml"`
+}
+
+// SetCustomizationScript ...
+func (g *GuestCustomizationSection) SetCustomizationScript(script string) {
+	g.CustomizationScript = &CustomizationScript{Value: script}
+}
+
+// GetCustomizationScript ...
+func (g *GuestCustomizationSection) GetCustomizationScript() string {
+	if g.CustomizationScript == nil {
+		return ""
+	}
+	return g.CustomizationScript.Value
 }
 
 // SetXMLNS : sets the xml namespace attributes for the request
